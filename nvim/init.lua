4eb9e921fd1f24da39f7ebcbd777954acc9b7017
Vojtech Mikes <vojtechmikes98@gmail.com>
2024-05-28 21:09:59 +0200

    Added fzf and new theme

--- nvim/init.lua
+++ nvim/init.lua
@@ -52,6 +52,10 @@ local plugins = {
 		'nvim-telescope/telescope.nvim',
 		branch = "0.1.x",
 		dependencies = { 'nvim-lua/plenary.nvim' }
+	},
+	{
+		'nvim-telescope/telescope-fzf-native.nvim', 
+		build = 'make' 
 	}
 }
 
@@ -73,9 +77,15 @@ require("nvim-treesitter.configs").setup {
 	indent = { enable = true },
 }
 
+require('telescope').setup()
+
+require('telescope').load_extension('fzf')
+
+
 local builtin = require('telescope.builtin')
 
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+
--- zshrc
+++ zshrc
@@ -1,5 +1,5 @@
 export ZSH="$HOME/.oh-my-zsh"
-ZSH_THEME="apple"
+ZSH_THEME="eastwood"
 plugins=(zsh-autosuggestions)
 alias vim=nvim
 source $ZSH/oh-my-zsh.sh
