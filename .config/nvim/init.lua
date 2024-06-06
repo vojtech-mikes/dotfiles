06c9336540b2803db367f5f3235b3a4e5414373d
Vojta <104932412+vojtech-mikes@users.noreply.github.com>
2024-06-06 10:40:18 +0200

    Update init.lua

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -261,7 +261,7 @@ require("telescope").load_extension("fzf")
 
 local builtin = require("telescope.builtin")
 
-vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
-vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
-vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
-vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
+vim.keymap.set("n", "<leader>sf", builtin.find_files, {})
+vim.keymap.set("n", "<leader>sg", builtin.live_grep, {})
+vim.keymap.set("n", "<leader>sb", builtin.buffers, {})
+vim.keymap.set("n", "<leader>sh", builtin.help_tags, {})
