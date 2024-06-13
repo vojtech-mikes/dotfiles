b27c5eb2250bf6149bdd46d19cfceb4a62ebfaa7
Vojta <104932412+vojtech-mikes@users.noreply.github.com>
2024-06-13 15:44:23 +0200

    Update .wezterm.lua

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,5 +1,25 @@
-local config = {
-	font_size = 16,
+local wezterm = require 'wezterm'
+local config = {}
+
+config.color_scheme = 'Hardcore'
+config.font_size = 12
+
+config.keys = {
+	{
+		key = 't',
+		mods = 'SHIFT|ALT',
+		action = wezterm.action.SpawnTab 'CurrentPaneDomain',
+	},
+	{
+		key = '"',
+		mods = 'SHIFT|ALT',
+		action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' }
+	},
+	{
+		key = '%',
+		mods = 'SHIFT|ALT',
+		action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' }
+	}
 }
 
 return config
