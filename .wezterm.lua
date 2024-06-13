39a38c17ed9a6987a5a0dd480cd5138f3f161d2a
Vojtech Mikes <vojtechmikes98@gmail.com>
2024-06-13 21:43:09 +0200

    font size change

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,7 +1,7 @@
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 local config = {}
 
-config.color_scheme = 'Hardcore'
-config.font_size = 12
+config.color_scheme = "Hardcore"
+config.font_size = 14
 
 return config
