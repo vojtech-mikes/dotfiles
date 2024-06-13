515a8a29102b390df3dda65902bd701fac963bcb
Vojta <104932412+vojtech-mikes@users.noreply.github.com>
2024-06-13 16:02:50 +0200

    Update .wezterm.lua
    Tmux is better for now

--- .wezterm.lua
+++ .wezterm.lua
@@ -4,22 +4,4 @@ local config = {}
 config.color_scheme = 'Hardcore'
 config.font_size = 12
 
-config.keys = {
-	{
-		key = 't',
-		mods = 'SHIFT|ALT',
-		action = wezterm.action.SpawnTab 'CurrentPaneDomain',
-	},
-	{
-		key = '"',
-		mods = 'SHIFT|ALT',
-		action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' }
-	},
-	{
-		key = '%',
-		mods = 'SHIFT|ALT',
-		action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' }
-	}
-}
-
 return config
