67287d2964c29d8632b1f44fef59a057d12c6985
Vojtech Mikes <vojtechmikes98@gmail.com>
2024-10-02 09:47:17 +0200

    Added comments from mini

--- .config/nvim/lua/plugins/comments.lua
+++ .config/nvim/lua/plugins/comments.lua
@@ -1,6 +1,5 @@
-return { 
-	'folke/todo-comments.nvim', 
-	event = 'VimEnter',
-	dependencies = { 'nvim-lua/plenary.nvim' },
-	opts = { signs = false } 
+return {
+	"echasnovski/mini.comment",
+	version = false,
+	opts = {},
 }
