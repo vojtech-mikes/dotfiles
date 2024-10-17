806c45664d402db380b84ee66e3b36528d2a5913
Vojtech Mikes <vojtechmikes98@gmail.com>
2024-10-17 12:07:51 +0200

    Added some config options to wezterm and Zed

--- .wezterm.lua
+++ .wezterm.lua
@@ -7,6 +7,13 @@ local config = wezterm.config_builder()
 -- Custom stuff
 
 config.font_size = 16
+config.enable_tab_bar = false
+config.window_background_opacity = 0.80
+
+config.color_scheme = 'Gruber (base16)'
+
+config.default_cursor_style = 'SteadyBlock'
+
 
 -- and finally, return the configuration to wezterm
 return config
