21f26f13d4b0f783f3f17cbc43bf2564dbb939d1
Vojtech Mikes <vojtechmikes98@gmail.com>
2024-10-21 21:41:32 +0200

    Added auto formatters and treesitter

---
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,25 @@
+return {
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
+	main = "nvim-treesitter.configs",
+	opts = {
+		ensure_installed = {
+			"python",
+			"bash",
+			"c",
+			"diff",
+			"html",
+			"lua",
+			"luadoc",
+			"markdown",
+			"markdown_inline",
+			"query",
+			"vim",
+			"vimdoc",
+		},
+		highlight = {
+			enable = true,
+		},
+		indent = { enable = true },
+	},
+}
