3319c0f6ca69b9d8f219b82780fc74fe0528c7ca
Vojtech Mikes <vojtechmikes98@gmail.com>
2024-10-24 21:29:56 +0200

    Added folkes todos

--- .config/nvim/lua/config/remap.lua
+++ .config/nvim/lua/config/remap.lua
@@ -8,3 +8,5 @@ vim.keymap.set("n", "<C-h>", "<C-w><C-h>")
 vim.keymap.set("n", "<C-l>", "<C-w><C-l>")
 vim.keymap.set("n", "<C-j>", "<C-w><C-j>")
 vim.keymap.set("n", "<C-k>", "<C-w><C-k>")
+
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
