e00285e8cdb4f3a8c82d4c8ca1452e5e702e4e49
Vojtech Mikes <vojtechmikes98@gmail.com>
2025-01-23 17:50:43 +0100

    Added status line and colorscheme

---
+++ nvim/.config/nvim/init.lua
@@ -0,0 +1,17 @@
+require("config.lazy")
+
+vim.opt.shiftwidth = 4
+vim.opt.clipboard = "unnamedplus"
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+--- For sourcing the config quickly, then comment it out
+vim.keymap.set("n", "<space><space>x", "<cmd>source %<CR>")
+
+vim.api.nvim_create_autocmd("TextYankPost", {
+  desc = "Highlight when yanking (copying) text",
+  group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end
+})
