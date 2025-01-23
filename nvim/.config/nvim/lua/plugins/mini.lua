24eb778848110c13970c722587843df66c667c03
Vojtech Mikes <vojtechmikes98@gmail.com>
2025-01-23 18:01:51 +0100

    Added Oil and keymaps file

--- nvim/.config/nvim/lua/plugins/mini.lua
+++ nvim/.config/nvim/lua/plugins/mini.lua
@@ -1,9 +1,11 @@
 return { 
-	'echasnovski/mini.nvim',
-	version = false,
-	enabled = true,
-	config = function()
-		local statusline = require "mini.statusline"
-		statusline.setup { use_icons = true }
-	end
+    'echasnovski/mini.nvim',
+    version = false,
+    enabled = true,
+    config = function()
+	local statusline = require "mini.statusline"
+	local icons = require "mini.icons"
+	statusline.setup { use_icons = true }
+	icons.setup {}
+    end
 }
