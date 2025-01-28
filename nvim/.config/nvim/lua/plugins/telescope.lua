1184e30b3b413e2c304f9b910800f49deadbd976
Vojtech Mikes <vojtechmikes98@gmail.com>
2025-01-28 10:51:28 +0100

    Added important mappings for telescope

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -7,9 +7,17 @@ return {
 
 	telescope.setup {}
 
-	vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
 	vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
 	vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
 	vim.keymap.set('n', '<leader>fo', builtin.oldfiles, { desc = 'Telescope oldfiles' })
+
+	vim.keymap.set('n', '<leader>ff', function ()
+	    builtin.find_files({hidden=true})
+	end, { desc = 'Telescope find files' })
+
+	vim.keymap.set('n', '<leader>fc', function ()
+	    builtin.find_files({cwd=vim.fn.stdpath('config')})
+	end, { desc = 'Telescope edit config files' })
+
     end
 }
