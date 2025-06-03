47dbf0f32108b016e34a7be5945c4e4888c228c3
Vojtech Mikes <vojtechmikes98@gmail.com>
2025-06-03 20:21:44 +0200

    Now I am using normal numbering

--- nvim/.config/nvim/lua/vojta/general.lua
+++ nvim/.config/nvim/lua/vojta/general.lua
@@ -1,7 +1,7 @@
 vim.opt.shiftwidth = 4
 vim.opt.clipboard = "unnamedplus"
 vim.opt.number = true
-vim.opt.relativenumber = true
+vim.opt.relativenumber = false
 vim.opt.cursorline = true
 vim.opt.scrolloff = 10
 vim.opt.inccommand = 'split'
