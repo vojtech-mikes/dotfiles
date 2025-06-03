5139344c0b7d0abfd5f71e38c164122cad0e7c5a
Vojtech Mikes <vojtechmikes98@gmail.com>
2025-06-03 20:05:41 +0200

    Added windows separators

--- nvim/.config/nvim/lua/vojta/general.lua
+++ nvim/.config/nvim/lua/vojta/general.lua
@@ -10,3 +10,20 @@ vim.opt.smartcase = true
 vim.opt.splitright = true
 vim.opt.splitbelow = true
 vim.opt.showmode = false
+vim.g.have_nerd_font = true
+
+
+vim.opt.laststatus = 3
+
+vim.opt.fillchars:append {
+  vert = '│',   -- Vertical line
+  horiz = '─',  -- Horizontal line
+  horizup = '┴',
+  horizdown = '┬',
+  vertleft = '┤',
+  vertright = '├',
+  verthoriz = '┼',
+}
+
+
+vim.api.nvim_set_hl(0, "WinSeparator", { fg = "white", bg = "none" })
