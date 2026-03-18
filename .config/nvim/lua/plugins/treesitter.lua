e413871868349ada9a21521116938fd2fe480708
Vojtech Mikes <vojtechmikes98@gmail.com>
2026-03-18 23:38:30 +0100

    Added xml parser

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -3,7 +3,7 @@ return {
     lazy = false,
     build = ':TSUpdate',
     config = function()
-	local parsers = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' , 'python'}
+	local parsers = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' , 'python', 'xml'}
 	require('nvim-treesitter').install(parsers)
 	vim.api.nvim_create_autocmd('FileType', {
 	    callback = function(args)
