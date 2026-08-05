1439a3908e92f1b590949d60db929aab8ce2ab2d
Vojtech Mikes <vojtechmikes98@gmail.com>
2026-08-05 13:09:39 +0200

    Refactored setup script to use dictionary, added mouse support to copy mode in tmux

--- setup.sh
+++ setup.sh
@@ -2,19 +2,19 @@
 
 echo "Setting up all the weak simlinks"
 
-files=("init.lua" "tmux.conf")
-
-declare -A dirs=( ["init.lua"]="nvim" ["tmux.conf"]="tmux" )
+declare -A files=( ["nvim"]="init.lua" ["tmux"]="tmux.conf" )
 
 script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 config_path="$HOME/.config"
 
-for file in "${files[@]}"; do
-  abs_path="$script_dir/$file"
-  dest_path="$config_path/${dirs[$file]}/$file"
-  echo "Creating simlink for: $abs_path -> $dest_path"
-  ln -s $abs_path $dest_path
+for file in "${!files[@]}"; do
+	val="${files[$file]}"
+	source_path="$script_dir/$val"
+	target_path="$config_path/$file/$val"
+	echo "Creating symlink for $source_path"
+	ln -s $source_path $target_path
 done
+
 
 echo "Done"
